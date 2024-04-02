de72d0533eb3d4e70bed73e81feb321e4cdd5efa
kozak-ruslan <kozakrus@gmail.com>
2024-04-02 18:09:38 +0300

    draft 01 - formatting and remove log

--- src/App.js
+++ src/App.js
@@ -1,92 +1,72 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import thunk from 'redux-thunk';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import thunk from "redux-thunk";
 
-import './App.css';
+import "./App.css";
 
-import Games from './components/games/Games';
-import UsersListHooks from './components/component-hooks/UsersListHook';
-import UsersList from './components/component-class/UsersList';
-import DemandSupply from './components/demand-supply/demand-supply';
+import Games from "./components/games/Games";
+import UsersListHooks from "./components/component-hooks/UsersListHook";
+import UsersList from "./components/component-class/UsersList";
+import DemandSupply from "./components/demand-supply/demand-supply";
 import TodoPromise from "./components/page-promise/promise-test";
-import Nav from './components/nav/nav';
-import { createStore, applyMiddleware } from 'redux'
+import Nav from "./components/nav/nav";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import InputPhone from "./components/phone-component/phones-list";
 import TodoComponent from "./components/todo-list/todi-component";
-import rootReducer from './reducers';
-import CreateProgrammer from './components/composition-extends/composition/create-prog';
-import Header from './components/header/header';
-
+import rootReducer from "./reducers";
+import CreateProgrammer from "./components/composition-extends/composition/create-prog";
+import Header from "./components/header/header";
 
 var store = createStore(rootReducer, applyMiddleware(thunk));
 
-
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <Header />
-        <Router>
-          <Nav />
-          <main>
-            <Switch>
-              <Route exact path='/' >
-                <h1>
-                  Home Page
-                </h1>
-              </Route>
-              <Route path='/games'>
-                <h1>
-                  Page Game
-                </h1>
-                <Games />
-              </Route>
-              <Route path='/hooks-user-list'>
-                <h1>
-                  Hooks
-                </h1>
-                <UsersListHooks />
-              </Route>
-              <Route path='/class-user-list'>
-                <h1>
-                  Class User List
-                </h1>
-                <UsersList />
-              </Route>
-              <Route path='/demand-supply'>
-                <h1>
-                  DemandSupply
-                </h1>
-                <DemandSupply />
-              </Route>
-              <Router path='/phone-component'>
-                <InputPhone />
-              </Router>
-              <Router path='/todo-component'>
-                <TodoComponent />
-              </Router>
-              <Router path='/promise'>
-                <TodoPromise />
-              </Router>
-              <Route path="/composition-extends">
-                <CreateProgrammer/>
-              </Route>
-              <Route path='*'>
-                <h1>
-                  404
-                </h1>
-              </Route>
-            </Switch>
-          </main>
-        </Router>
-      </div>
-    </Provider>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <Header />
+      <Router>
+        <Nav />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <h1>Home Page</h1>
+            </Route>
+            <Route path="/games">
+              <h1>Page Game</h1>
+              <Games />
+            </Route>
+            <Route path="/hooks-user-list">
+              <h1>Hooks</h1>
+              <UsersListHooks />
+            </Route>
+            <Route path="/class-user-list">
+              <h1>Class User List</h1>
+              <UsersList />
+            </Route>
+            <Route path="/demand-supply">
+              <h1>DemandSupply</h1>
+              <DemandSupply />
+            </Route>
+            <Router path="/phone-component">
+              <InputPhone />
+            </Router>
+            <Router path="/todo-component">
+              <TodoComponent />
+            </Router>
+            <Router path="/promise">
+              <TodoPromise />
+            </Router>
+            <Route path="/composition-extends">
+              <CreateProgrammer />
+            </Route>
+            <Route path="*">
+              <h1>404</h1>
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  </Provider>
+);
 
 export default App;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,85 +1,79 @@
-import React, { useState }  from 'react';
-import './home.css'
-import UsersList from './UsersList';
-import UsersHooks from './UsersListHook';
-import UsersHooksStupid from './UsersListHookStupid';
+import React, { useState } from "react";
+import "./home.css";
+import UsersList from "./UsersList";
+import UsersHooks from "./UsersListHook";
+import UsersHooksStupid from "./UsersListHookStupid";
 
-function Square(props){
-  return(
-    <button className='square' onClick={() => props.onClick()}>
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
       {props.count}
     </button>
-    )
+  );
 }
 
 function PreviousStep(props) {
- return (
-   <button className='previous-btn' onClick={() => props.onClick()}> {'<-'} </button>
- ) 
+  return (
+    <button className="previous-btn" onClick={() => props.onClick()}>
+      {"<-"}
+    </button>
+  );
 }
 function HooksStates() {
-  
   const [count, setCount] = useState(0);
-  return(
+  return (
     <div>
-      <p>Вы кликнули {count} раз</p>
-      <button onClick={ () => setCount(count + 1)}> Hooks set</button>
+      <p>You click {count} times</p>
+      <button onClick={() => setCount(count + 1)}> Hooks set</button>
     </div>
-  )
+  );
 }
 
-
-export class Board extends React.Component{
-
+export class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
-    }
+    };
     this.history = [];
   }
 
   handleClick(i) {
     // const squares = this.state.squares.slice();
-    const { 
-      squares,
-      xIsNext,
-      squ
-    } = this.state;
+    const { squares, xIsNext, squ } = this.state;
     console.log(xIsNext);
     console.log(squ);
     if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext? 'X': 'O';
-    this.setState(
-      {
-        squares: squares,
-        xIsNext: !this.state.xIsNext
-      });
-      this.history.push(squares);
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+    this.history.push(squares);
   }
-  
+
   renderSquare(i) {
-    return <Square
-      count={ this.state.squares[i] }
-      onClick={ () => this.handleClick(i) }/>
+    return (
+      <Square
+        count={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   handleBackStep() {
-    if(this.history.length && this.history.length > 0 ) {
-      const index = this.history.length -1;
+    if (this.history.length && this.history.length > 0) {
+      const index = this.history.length - 1;
       const squares = this.history[index - 1].slice();
-      this.setState(
-        {
-          squares: squares,
-          xIsNext: !this.state.xIsNext
-        });
-  
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   }
-
 
   calculateWinner(squares) {
     const lines = [
@@ -94,33 +88,35 @@ export class Board extends React.Component{
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
         return squares[a];
       }
     }
     return null;
-   }
+  }
 
   render() {
-    const winner = this.calculateWinner(this.state.squares)
+    const winner = this.calculateWinner(this.state.squares);
 
-    const title = 'base class Board';
+    const title = "base class Board";
     let status;
-      if(winner) {
-        status = 'Победитель: ' + winner;
-      } else {
-        status = 'Следующий игрок: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-    
-
+    if (winner) {
+      status = "Победитель: " + winner;
+    } else {
+      status = "Следующий игрок: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
-        <div className='name'>
+        <div className="name">
           {title} , {status}
         </div>
         <div>
-          <PreviousStep onClick={() => this.handleBackStep()}/>
+          <PreviousStep onClick={() => this.handleBackStep()} />
         </div>
         <div>
           <div className="board-row">
@@ -139,71 +135,54 @@ export class Board extends React.Component{
             {this.renderSquare(8)}
           </div>
         </div>
-        <div>Test Hooks</div>        
+        <div>Test Hooks</div>
         <HooksStates></HooksStates>
-      </div>      
+      </div>
     );
   }
-
 }
-
-
-
-
-
 
 export class Home extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.onChange = this.onChange.bind(this);
-    this.state={
+    this.state = {
       users: [
-        {name: 'Vasya', age: 50},
-        {name: 'Ivan', age: 20},
-        {name: 'Larisa', age: 25}, 
-      ]
-    }
+        { name: "Vasya", age: 50 },
+        { name: "Ivan", age: 20 },
+        { name: "Larisa", age: 25 },
+      ],
+    };
   }
 
   onChange(e, i) {
     const value = e.target.value;
     const { users } = this.state;
     users[i].age = +value;
-    this.setState({users});
-};
+    this.setState({ users });
+  }
 
   render() {
-    const {users} = this.state;
+    const { users } = this.state;
     return (
       <div className="game">
-        <h1>
-          Page Game
-        </h1>
+        <h1>Page Game</h1>
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-        </div>
-        <br/>
+        <div className="game-info"></div>
+        <br />
         <div> Class User List</div>
         <UsersList />
-        <br/>
-        
-        <UsersHooksStupid
-          users={users}
-          onChange={this.onChange}
-        />
-        <br/>
+        <br />
+        <UsersHooksStupid users={users} onChange={this.onChange} />
+        <br />
         11111
-        <br/>
-        <UsersHooks/>
+        <br />
+        <UsersHooks />
       </div>
     );
   }
 }
 
-
-
 export default Home;
-
-
